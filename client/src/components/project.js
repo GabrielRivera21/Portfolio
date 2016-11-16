aabdcd8a532be5b132115d3529b1a77be4d21cb1
Rivera <gabriel.a.rivera@accenture.com>
2016-11-15 17:14:14 -0800

    started adding project components

--- client/src/components/project.js
+++ client/src/components/project.js
@@ -1,11 +1,69 @@
 import React from 'react';
 
+import Config from '../config/config';
+
+class ProjectData extends React.Component {
+    constructor(props) {
+      super(props);
+      this.loadProjectsFromServer = this.loadProjectsFromServer.bind(this);
+      this.state = { data: [], loaded: false, failedToLoad: false };
+    }
+    componentDidMount() {
+      this.loadProjectsFromServer();
+    }
+    loadProjectsFromServer() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        success: function(response) {
+          this.setState({data: response.results, loaded: true});
+        }.bind(this),
+        error: function(xhr, status, error) {
+          this.setState({loaded: true, failedToLoad: true});
+          console.error(this.props.url, status, error.toString());
+        }.bind(this)
+      });
+    }
+    render() {
+      return (
+        <div className="projects">
+          <ProjectList data={this.state.data} />
+        </div>
+      )
+    }
+};
+
+class ProjectList extends React.Component {
+  render() {
+    let projectList = this.props.data.map((project) => {
+      return (
+        <ProjectItem
+          key={project.id} />
+      )
+    });
+    return (
+      <div className="project-list">
+        {projectList}
+      </div>
+    );
+  }
+};
+
+class ProjectItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return null;
+  }
+};
+
 class Project extends React.Component {
   render() {
     return (
-      <div className="projects centered">
-        <h1>Project Page</h1>
-        <h2>Page under construction</h2>
+      <div className="projects">
+        <h1 className="centered">Project Page</h1>
+        <ProjectData url={`${Config.Api}/api/projects`}
       </div>
     );
   }
